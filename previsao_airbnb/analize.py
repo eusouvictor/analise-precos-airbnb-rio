3fbcb7c25c7fb7266bff0adb92236ea6b336a46f
Victor Andrade Domingos da Silva <vito.andrade01@gmail.com>
2025-11-17 16:07:05 -0300

    ...

--- previsao_airbnb/analize.py
+++ previsao_airbnb/analize.py
@@ -1,5 +1,11 @@
+import os
 import pandas as pd
 
-df = pd.read_csv('listings.csv.gz')
+# Caminho absoluto para o arquivo
+file_path = os.path.join(os.getcwd(), 'previsao_airbnb', 'listings.csv.gz')
+if not os.path.exists(file_path):
+    raise FileNotFoundError(f"O arquivo '{file_path}' não foi encontrado. Certifique-se de que ele está no local correto.")
+
+df = pd.read_csv(file_path)
 
 print(df.head())
